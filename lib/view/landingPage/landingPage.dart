acb211c47cc7f383960efe5da6af358591c25669
Abdulrahman Ashraf <abdalrhmanashraf00@gmail.com>
2025-07-09 20:17:02 +0300

    fix bug #1

--- lib/view/landingPage/landingPage.dart
+++ lib/view/landingPage/landingPage.dart
@@ -22,45 +22,47 @@ class Landingpage extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.only(top: 30.0),
-          child: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(44),
-            child: Column(
-              children: [
-                Text(
-                  "Unlock Your Potential opportunities with an Internship at Qayedny",
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..color=primaryColor
-                      ..strokeWidth=2,
-                    fontSize: 22
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0),
-                  child: Text(
-                    "At Qayedny, we believe that internships are the gateway to a successful career. Joining our internship can make a significant impact on your professional journey",
+          child: SingleChildScrollView(
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(44),
+              child: Column(
+                children: [
+                  Text(
+                    "Unlock Your Potential opportunities with an Internship at Qayedny",
                     style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                        foreground: Paint()
-                          ..style = PaintingStyle.stroke
-                          ..color=primaryColor
-                          ..strokeWidth=2,
-                        fontSize: 22
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..color=primaryColor
+                        ..strokeWidth=2,
+                      fontSize: 22
                     ),
                   ),
-                ),
-                SizedBox(height: 250),
-                Column(
-                  children: [
-                    CustomTextButton("Subscribe now", Colors.white),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: CustomTextButton("Explore", Colors.grey),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Text(
+                      "At Qayedny, we believe that internships are the gateway to a successful career. Joining our internship can make a significant impact on your professional journey",
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..color=primaryColor
+                            ..strokeWidth=2,
+                          fontSize: 22
+                      ),
                     ),
-                  ],
-                )
-              ],
+                  ),
+                  SizedBox(height: 250),
+                  Column(
+                    children: [
+                      CustomTextButton("Subscribe now", Colors.white),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: CustomTextButton("Explore", Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
